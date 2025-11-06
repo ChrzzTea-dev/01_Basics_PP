1fad0c2cd3ceca92af60c3d7305e699ecd45cb40
ChrzzTea-dev <ChrzzTeagithub@gmx.de>
2025-11-06 10:52:48 +0000

    II+

--- src/02_variablen.js
+++ src/02_variablen.js
@@ -23,3 +23,35 @@ console.log("Inhalt:" + firstName); // Ausgabe
 let familyName = "Mütze"; // Deklaration & Wertzuweisung
 console.log("Vor und zu Name:" + firstName + " " + familyName);// Ausgabe --> Vor und zu Name Chris Mütze
 
+/**
+ * Wertzuweisung II + Datenypen
+ * JS ist ein untypisierte Sprache!
+ */
+
+
+
+
+
+let test;
+
+/* undefined */
+console.log("Datentyp: " + typeof test); // Ausgabe Datentyp
+console.log("Inhalt: " + test); // Ausgabe Inhalt
+console.log("----------------")
+
+
+
+test = "hi"; // string
+console.log("Datentyp: " + typeof test); // Ausgabe Datentyp
+console.log("Inhalt: " + test); // Ausgabe Inhalt
+console.log("----------------")
+
+test = 2; // number
+console.log("Datentyp: " + typeof test); // Ausgabe Datentyp
+console.log("Inhalt: " + test); // Ausgabe Inhalt
+console.log("----------------")
+
+test = true; // boolean
+console.log("Datentyp: " + typeof test); // Ausgabe Datentyp
+console.log("Inhalt: " + test); // Ausgabe Inhalt
+console.log("----------------")
