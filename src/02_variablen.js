b0bf9c02e4c59a1fe4d5dcf08fb82bc37037ff08
ChrzzTea-dev <ChrzzTeagithub@gmx.de>
2025-11-06 10:13:03 +0000

    2 Test

--- src/02_variablen.js
+++ src/02_variablen.js
@@ -11,7 +11,15 @@
 
 /***** Variablen 01 *******/
 
+//consol.log("Hi!")
 
 
+/**Deklaration + Wertzuweisung I */
 
+let firstName; // Deklaration (Definition)
+console.log(firstName);// Ausgabe 
+firstName = "Chris"; // Wertzuweisung / assignment
+console.log("Inhalt:" + firstName); // Ausgabe 
+let familyName = "Mütze"; // Deklaration & Wertzuweisung
+console.log("Vor und zu Name:" + firstName + " " + familyName);// Ausgabe --> Vor und zu Name Chris Mütze
 
