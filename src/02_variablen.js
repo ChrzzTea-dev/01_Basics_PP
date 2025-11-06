3c237494c836b56b8075ea288c28a2b91268100c
ChrzzTea-dev <ChrzzTeagithub@gmx.de>
2025-11-06 11:42:27 +0000

    03 Prompt

--- src/02_variablen.js
+++ src/02_variablen.js
@@ -16,8 +16,8 @@
 
 /**Deklaration + Wertzuweisung I */
 
-let firstName; // Deklaration (Definition)
-console.log(firstName);// Ausgabe 
+//let firstName; // Deklaration (Definition)
+/**console.log(firstName);// Ausgabe 
 firstName = "Chris"; // Wertzuweisung / assignment
 console.log("Inhalt:" + firstName); // Ausgabe 
 let familyName = "Mütze"; // Deklaration & Wertzuweisung
@@ -32,26 +32,40 @@ console.log("Vor und zu Name:" + firstName + " " + familyName);// Ausgabe --> Vo
 
 
 
-let test;
+//let test;
 
 /* undefined */
-console.log("Datentyp: " + typeof test); // Ausgabe Datentyp
-console.log("Inhalt: " + test); // Ausgabe Inhalt
-console.log("----------------")
+//console.log("Datentyp: " + typeof test); // Ausgabe Datentyp
+//console.log("Inhalt: " + test); // Ausgabe Inhalt
+//console.log("----------------")
 
 
 
-test = "hi"; // string
-console.log("Datentyp: " + typeof test); // Ausgabe Datentyp
-console.log("Inhalt: " + test); // Ausgabe Inhalt
-console.log("----------------")
+//test = "hi"; // string
+//console.log("Datentyp: " + typeof test); // Ausgabe Datentyp
+//console.log("Inhalt: " + test); // Ausgabe Inhalt
+//console.log("----------------")
 
-test = 2; // number
-console.log("Datentyp: " + typeof test); // Ausgabe Datentyp
-console.log("Inhalt: " + test); // Ausgabe Inhalt
-console.log("----------------")
+//test = 2; // number
+//console.log("Datentyp: " + typeof test); // Ausgabe Datentyp
+//console.log("Inhalt: " + test); // Ausgabe Inhalt
+//console.log("----------------")
 
-test = true; // boolean
-console.log("Datentyp: " + typeof test); // Ausgabe Datentyp
-console.log("Inhalt: " + test); // Ausgabe Inhalt
-console.log("----------------")
+//test = true; // boolean
+//console.log("Datentyp: " + typeof test); // Ausgabe Datentyp
+//console.log("Inhalt: " + test); // Ausgabe Inhalt
+//console.log("----------------")
+
+
+/* Variablen vs. Konstanten in JS */
+
+// Variable
+let test; // Variable : Deklaration
+test = "hi"; // Wertzuweisung (Initialisierung)
+test = "hello"; // Überschreiben
+console.log("Inhalt: " + test); // Ausgabe 
+
+// Konstante
+const test2 = "HI!"; // Deklaration + Wertzuweisung gemeinsam !
+//test2 = "hello";// Laufzeit-Fehler!!
+console.log("Inhalt: " + test2); //Ausgabe
