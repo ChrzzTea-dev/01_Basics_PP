f7376b89227a937c34e960e66ca3471d4c8bac53
ChrzzTea-dev <ChrzzTeagithub@gmx.de>
2025-11-10 11:56:00 +0000

    07/ code challenge

---
+++ 07_codechallenge_II.js
@@ -0,0 +1,40 @@
+/*********   CODE CHALLENGE 02 **********/
+
+/*
+Geben über eine Prompt-Anforderung einen Namen ein
+Geben über eine Prompt-Anforderung ein Alter ein
+
+Geben Sie in Abhängigkeit vom angegebenen Alter 
+folgende Getränkeauswahl in die Konsole aus :
+
+Alter 0-5 // Milch  --> age > 0 AND age < 6
+Alter 6-12 // Saft
+Alter 13-17 // Cola
+18 + // Wein
+
+-->zB: "Max trinkt Cola."
+
+/* Getränke-Challenge */
+
+const prompt = require('prompt-sync')({sigint: true});// für die eingabe für Prompts
+
+let kname = prompt("Name: ? ");
+let kalter = prompt("Alter: ? ");
+
+
+if(kalter >0, kalter<6)
+{
+console.log("bekommt Milch")
+}
+else if (kalter>=6, kalter<12)
+{
+console.log("bekommt Saft")
+}
+else if (kalter >=13, kalter<17)
+{
+console.log("bekommt Cola")
+}
+else
+{
+console.log("bekommt Wein")
+}
