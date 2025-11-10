4365d3ca315c6e678434929feb71f7161e414178
ChrzzTea-dev <ChrzzTeagithub@gmx.de>
2025-11-10 09:13:27 +0000

    06/ Ternäre Schreibweise

--- 06_controlflow.js
+++ 06_controlflow.js
@@ -1,7 +1,7 @@
 // Entscheidungsstrukturen | control flow
 
 // Deklaration + Assignment
-const ageJohn = 32;
+const ageJohn = 25;
 const ageMark = 30;
 
 // Deklaration
@@ -27,9 +27,26 @@ console.log("ageMark: " + ageMark);
 //if(true)             // Wahrheitswert(e)
 //if(false)
 //if(isJohnOlder)      // Verweis auf in Wahrheitswert 
-if(ageJohn > ageMark)  // direkter Test 
+/*if(ageJohn > ageMark)  // direkter Test 
 {
     console.log("John ist älter");
+}/
+
+
+/************ IF - ELSE  ************/
+// mit Alternative  
+// entweder ja oder nein
+
+
+/*if (ageJohn == ageMark) {
+    console.log("John ist älter")
+} else {
+    console.log("John ist jünger")
 }
 
 
+********  Ternäre (ternary) Schreibweise ************/
+// console.log( (isJohnOlder) ? "John is older" : "Mark is older");
+
+/************ IF - ELSE IF  ************/
+// mit alternativen Fällen (älter, jünger, gleich alt)
